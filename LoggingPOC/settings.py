a6da7d8f62b3ed61e22326b4000081cb5a705653
durgesh <durgehs.tiwari@ongraph.com>
2020-09-22 18:52:25 +0530

    Logging Poc

--- LoggingPOC/settings.py
+++ LoggingPOC/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'logapp',
 ]
 
 MIDDLEWARE = [
@@ -102,85 +103,85 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'formatters': {
-#         'verbose': {
-#             'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-#         },
-#         'simple': {
-#             'format': '%(levelname)s %(message)s'
-#         },
-#     },
-#     'filters': {
-#         # 'special': {
-#         #     '()': 'project.logging.SpecialFilter',
-#         #     'foo': 'bar',
-#         # },
-#         'require_debug_true': {
-#             '()': 'django.utils.log.RequireDebugTrue',
-#         },
-#     },
-#     'handlers': {
-#         'console': {
-#             'level': 'INFO',
-#             'filters': ['require_debug_true'],
-#             'class': 'logging.StreamHandler',
-#             'formatter': 'simple'
-#         },
-#         'file': {
-#             'level': 'INFO',
-#             'class': 'logging.handlers.TimedRotatingFileHandler',
-#             'filename': os.path.join(BASE_DIR, 'logs/debug.log'),
-#             # 'filename': '/opt/python/log/debug.log',
-#             'when': 'D',  # this specifies the interval
-#             # 'filename': 'D:\loggingpoc\loggingrequest.log',
-#             'interval': 1,  # defaults to 1, only necessary for other values
-#             'backupCount': 10,  # how many backup file to keep, 10 days
-#             'formatter': 'verbose'
-#         },
-#         'request_file': {
-#             'level': 'INFO',
-#             'class': 'logging.handlers.TimedRotatingFileHandler',
-#             'filename': os.path.join(BASE_DIR, 'logs/debug.log'),
-#             # 'filename': 'D:\loggingpoc\loggingequest.log',
-#             'when': 'D',  # this specifies the interval
-#             'interval': 1,  # defaults to 1, only necessary for other values
-#             'backupCount': 10,  # how many backup file to keep, 10 days
-#             # 'maxBytes': 1024*1024*10,
-#             'formatter': 'verbose'
-#         },
-#         'mail_admins': {
-#             'level': 'ERROR',
-#             'class': 'django.utils.log.AdminEmailHandler',
-#             # 'filters': ['special']
-#         }
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['console', 'file'],
-#             'propagate': True,
-#             'level': 'DEBUG',
-#         },
-#         'django.request': {
-#             'handlers': ['mail_admins','file'],
-#             # 'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
-#             'level': 'DEBUG',
-#             'propagate': False,
-#         },
-#         'LoggingPOC.logging_middleware': {
-#             'handlers': ['request_file'],
-#             'level': 'DEBUG',
-#             'propagate': True,
-#         },
-#         'django_eb.utils': {
-#             'handlers': ['console', 'file'],
-#             'propagate': True,
-#             'level': 'DEBUG',
-#         },
-#     }
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'filters': {
+        # 'special': {
+        #     '()': 'project.logging.SpecialFilter',
+        #     'foo': 'bar',
+        # },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/debug.log'),
+            # 'filename': '/opt/python/log/debug.log',
+            'when': 'D',  # this specifies the interval
+            # 'filename': 'D:\loggingpoc\loggingrequest.log',
+            'interval': 1,  # defaults to 1, only necessary for other values
+            'backupCount': 10,  # how many backup file to keep, 10 days
+            'formatter': 'verbose'
+        },
+        'request_file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/debug.log'),
+            # 'filename': 'D:\loggingpoc\loggingequest.log',
+            'when': 'D',  # this specifies the interval
+            'interval': 1,  # defaults to 1, only necessary for other values
+            'backupCount': 10,  # how many backup file to keep, 10 days
+            # 'maxBytes': 1024*1024*10,
+            'formatter': 'verbose'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            # 'filters': ['special']
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'django.request': {
+            'handlers': ['mail_admins','file'],
+            # 'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'LoggingPOC.logging_middleware': {
+            'handlers': ['request_file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django_eb.utils': {
+            'handlers': ['console', 'file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -200,3 +201,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '' #sender's email-id
+EMAIL_HOST_PASSWORD = '' #password associated with above email-id
--- logapp/views.py
+++ logapp/views.py
@@ -1,3 +1,42 @@
+import sys
+import datetime
+
+from django.core.mail import send_mail
+from LoggingPOC import settings
 from django.shortcuts import render
 
 # Create your views here.
+# import the logging library
+import logging
+from django.http import HttpResponse
+
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+def Test(request):
+    try:
+        # import pdb;
+        # pdb.set_trace()
+        print('Hello')
+        c='a'/10
+    except Exception as e:
+
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        logger.error(exc_type)
+        sendSimpleMail("Internal Error", 'exc_type',settings.EMAIL_HOST_USER,'durgesh.ccts@gmail.com')
+        traceback_details = {
+            'file_name': exc_traceback.tb_frame.f_code.co_filename,
+            'line_no': exc_traceback.tb_lineno,
+            'name': exc_traceback.tb_frame.f_code.co_name,
+            'type': exc_type.__name__,
+            'message': str(e),  # or see traceback._some_str()
+            'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+
+    return HttpResponse('In Test')
+
+
+
+def sendSimpleMail(subject,msg,emailfrom,emailto):
+    send_mail(subject,msg, emailfrom, [emailto],fail_silently = False)
